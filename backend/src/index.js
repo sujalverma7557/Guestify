cb183e9c2199070b226ca06b23e8800a9be7b101
Sujal Verma <e22cseu1464@bennett.edu.in>
2026-04-08 16:41:56 +0530

    fix: expose server to internet

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,6 +31,6 @@ app.use("/api/visitors", visitorRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/visitrequests", visitRequestRoute);
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
